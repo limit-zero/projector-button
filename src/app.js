703efb77494b58ab8c0907223f16e201f4c15db8
Jacob Bare <jacob@limit0.io>
2019-04-25 17:25:23 -0500

    Add status check and power on function

--- src/app.js
+++ src/app.js
@@ -1,12 +1,49 @@
+const cheerio = require('cheerio');
 const express = require('express');
-const { USERNAME, PASSWORD } = require('./env');
+const fetch = require('node-fetch');
+const { asyncRoute } = require('@base-cms/utils');
+const { USERNAME, PASSWORD, PROJECTOR_URI } = require('./env');
 
 const app = express();
 
 const createAuth = () => Buffer.from(`${USERNAME}:${PASSWORD}`).toString('base64');
 
-app.get('/', (req, res) => {
-  res.json({ ping: 'pong', auth: createAuth() });
-});
+const checkStatus = async () => {
+  const query = 'key=pow_on&lang=e&from=BASIC_CTL';
+  const response = await fetch(`${PROJECTOR_URI}/cgi-bin/power_ctl.cgi?${query}`, {
+    method: 'get',
+    headers: {
+      Authorization: `Basic ${createAuth()}`,
+    },
+  });
+  if (!response.ok) throw new Error(response.statusText);
+  const body = await response.text();
+  const $ = cheerio.load(body);
+  return $('body').text().trim();
+};
+
+const powerOn = async () => {
+  const response = await fetch(`${PROJECTOR_URI}/cgi-bin/power_on.cgi`, {
+    method: 'post',
+    headers: {
+      Authorization: `Basic ${createAuth()}`,
+    },
+    body: 'lang=e&from=BASIC_CTL',
+  });
+  if (!response.ok) throw new Error(response.statusText);
+  const body = await response.text();
+  const $ = cheerio.load(body);
+  return $('body').text().trim();
+};
+
+app.get('/', asyncRoute(async (req, res) => {
+  const status = await checkStatus();
+  if (status !== 'Are you sure to let projector power on ?') {
+    res.send('The projector is already on.');
+  } else {
+    const result = await powerOn();
+    res.send(result);
+  }
+}));
 
 module.exports = app;
